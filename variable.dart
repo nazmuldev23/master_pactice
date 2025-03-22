f843e25d1f8943d9d94650879e006ef96834c1bc
nazmuldev23 <168578321+nazmuldev23@users.noreply.github.com>
2025-03-22 17:08:52 +0600

    Add files via upload

---
+++ variable.dart
@@ -0,0 +1,6 @@
+main(){
+  int roll = 100; // 1,2,3,4,5,6,7,8,9.......
+  String name = "Nazmul"; // all text value
+  double result = 4.58;
+  print("$name\n$roll\n$result"); //variables value print
+}
