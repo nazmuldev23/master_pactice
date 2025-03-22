457a8f940dce21ed1afc08a9b3620d183912d351
nazmuldev23 <168578321+nazmuldev23@users.noreply.github.com>
2025-03-22 16:41:09 +0600

    Update loop.dart

--- loop.dart
+++ loop.dart
@@ -1,5 +1,19 @@
 void main() {
+  //for loop
   for (int i = 1; i <= 10; i++) {
     print("$i");
   }
+  //while loop
+  int i = 1; //data type
+  while (i <= 10) {
+    //loop
+    print("$i"); // kaj
+    i++; // incriment /dicrement
+  }
+  // do while loop
+  int k = 1;
+  do {
+    print("$k"); //kaj
+    k++; //incriment /dicriment
+  } while (k <= 10); //loop
 }
