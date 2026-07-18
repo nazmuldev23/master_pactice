00f7780cfaf5a34924073d2d7a15e653932c3444
nazmuldev23 <nazmulhossen23.bd@gmail.com>
2026-07-18 09:31:56 +0600

    data type

---
+++ lib/data_type.dart
@@ -0,0 +1,8 @@
+void main(){
+  int number = 18;//int
+  double result = 4.80;//double
+  num hight = 5.6; //int or double
+  String name = 'Nazmul';//String
+  bool isLogin = true;
+  print("Hi I am $name.I am $number years old.My result is $result,My hight $hight,MY id $isLogin");
+}
