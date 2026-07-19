c23bbd7db438d98c2d36d53f536892af73530fcd
nazmuldev23 <nazmulhossen23.bd@gmail.com>
2026-07-19 09:35:43 +0600

    Varilable data type

---
+++ lib/data_type.dart
@@ -0,0 +1,9 @@
+void main(){
+  int number = 18;//int
+  double result = 4.80;//double
+  num hight = 5.6; //int or double
+  String name = 'Nazmul';//String
+  bool isLogin = true;
+  dynamic address = 'dhaka';
+  print("Hi I am $name.I am $number years old.My result is $result,My hight $hight,MY id $isLogin address $address");
+}
