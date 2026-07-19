d9d7aefa7176f1ba925dfe56f6f7813307ca6235
nazmuldev23 <nazmulhossen23.bd@gmail.com>
2026-07-19 11:07:08 +0600

    big,small & same number

--- lib/big_small.dart
+++ lib/big_small.dart
@@ -11,8 +11,10 @@ void main(){
 
   if(num1 < num2){
     print('The big number is $num2');
-  } else if(num1 > num2){
     print('The small number is $num1');
+  } else if(num1 > num2){
+    print('The big number is $num1');
+    print('The small number is $num2');
   } else {
     print('Two number is same');
   }
